6953943c94358985ab46e6043b729ef11d13cf25
João Taranto <tarantoj04@gmail.com>
2024-09-29 11:11:04 -0300

    feat: ensure that tasks will not lose with LocalStorage

--- script.js
+++ script.js
@@ -39,10 +39,13 @@ const handleAddTask = () => {
     tasksContainer.appendChild(taskItemContainer)
 
     inputElement.value = ""
+
+    updateLocalStorage();
 }
+let itemCheck;
 
 const handleCheckClick = (taskContent) => {
-    const itemCheck = document.querySelector('.action-task').firstChild;
+    itemCheck = document.querySelector('.action-task').firstChild;
     const tasks = tasksContainer.childNodes;
     for (const task of tasks) {
         const currentTaskIsBeingClicked = task.firstChild.isSameNode(taskContent)
@@ -50,7 +53,9 @@ const handleCheckClick = (taskContent) => {
             task.firstChild.classList.toggle('completed')
             itemCheck.classList.toggle('icon-completed')
         }
+        
     }
+    updateLocalStorage();
 }
 
 const handleDeleteClick = (taskItemContainer, taskContent) => {
@@ -62,6 +67,8 @@ const handleDeleteClick = (taskItemContainer, taskContent) => {
             taskItemContainer.remove()
         }
     }
+    updateLocalStorage();
+
 }
 
 const handleInputChange = () => {
@@ -71,6 +78,66 @@ const handleInputChange = () => {
         return inputElement.classList.remove("error");
     }
 }
+
+const updateLocalStorage = () => {
+    const tasks = tasksContainer.childNodes;
+    
+    const localStorageTasks = [...tasks].map((task) => {
+        const content = task.firstChild;
+        let isCompleted = content.classList.contains('completed');
+        let isChecked = task.querySelector('.fa-square-check').classList.contains('icon-completed')
+
+        return { description: content.innerText, isCompleted, isChecked };
+    })
+
+    
+    localStorage.setItem("tasks", JSON.stringify(localStorageTasks))
+}
+
+const refreshTasksLocalStorage = (updateLocalStorage) => {
+    
+    
+    
+    const tasksFromLocalStorage = JSON.parse(localStorage.getItem('tasks'));
+    for (const task of tasksFromLocalStorage) {
+                const taskItemContainer = document.createElement('div');
+        taskItemContainer.classList.add('task-item');
+
+        const taskContent = document.createElement('p');
+        taskContent.innerText = task.description;
+        if (task.isCompleted) {
+            taskContent.classList.add('completed');
+        } 
+
+        const actionTask = document.createElement('div')
+        actionTask.classList.add('action-task')
+
+
+        const checkItem = document.createElement('i')
+        checkItem.classList.add('fa-solid')
+        checkItem.classList.add('fa-square-check')
+        if (task.isChecked) {
+            checkItem.classList.toggle('icon-completed')
+        }
+        
+        
+        const deleteItem = document.createElement('i')
+        deleteItem.classList.add('fa-solid')
+        deleteItem.classList.add('fa-trash-can')
+        deleteItem.addEventListener('click', () => handleDeleteClick(taskItemContainer, taskContent))
+
+        taskItemContainer.appendChild(taskContent)
+        taskItemContainer.appendChild(actionTask)
+        actionTask.appendChild(checkItem)
+        actionTask.appendChild(deleteItem)
+
+        tasksContainer.appendChild(taskItemContainer)
+
+        
+    }
+};
+
+refreshTasksLocalStorage();
 
 addTaskButton.addEventListener("click", () => handleAddTask());
 
